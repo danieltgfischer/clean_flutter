763d96fc44812d3c1e9de1bb7238184767b6f8a0
Daniel Fischer <danieltgfischer@gmail.com>
2024-08-31 20:37:35 -0300

    test: ensure RemoteAuthentication returns an AccountEntity on 200

--- test/data/use_cases/remote_authentication_test.dart
+++ test/data/use_cases/remote_authentication_test.dart
@@ -35,6 +35,15 @@ void main() {
   });
 
   test("Should call HttpClient with correct values", () async {
+    when(
+      () => httpClient.request(
+        url: any(named: 'url'),
+        method: any(named: 'method'),
+        body: any(named: 'body'),
+      ),
+    ).thenAnswer((_) async =>
+        {'accessToken': faker.guid.guid(), 'name': faker.person.name()});
+
     await sut.auth(params);
 
     verify(
@@ -100,5 +109,21 @@ void main() {
     final future = sut.auth(params);
 
     expect(future, throwsA(DomainError.invalidCredentials));
+  });
+
+  test("Should returns AccountEntity if HttpClient returns 200", () async {
+    final accessToken = faker.guid.guid();
+    when(
+      () => httpClient.request(
+        url: any(named: 'url'),
+        method: any(named: 'method'),
+        body: any(named: 'body'),
+      ),
+    ).thenAnswer(
+        (_) async => {'accessToken': accessToken, 'name': faker.person.name()});
+
+    final account = await sut.auth(params);
+
+    expect(account.token, accessToken);
   });
 }
