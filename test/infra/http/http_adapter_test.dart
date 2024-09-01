1313d761b17df2bdd85ad5f3d8d952f8f9fac01a
Daniel Fischer <danieltgfischer@gmail.com>
2024-09-01 20:06:25 -0300

    test: ensure HttpAdapter handle errors

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,4 @@
+import 'package:clean_flutter/data/http/http_error.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
@@ -28,11 +29,16 @@ void main() {
           body: any(named: 'body'),
         ));
 
-    void mockAnswer(String? data, int statusCode) =>
+    void mockAnswer(int statusCode, {String? data}) =>
         mockRequest().thenAnswer((_) async => Response(data ?? '', statusCode));
+
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       anyAnswer = '{"any_key": "any_value"}';
-      mockAnswer(null, 200);
+      mockAnswer(200);
     });
     test('Should call post with correct values', () async {
       final headers = {
@@ -60,27 +66,86 @@ void main() {
     });
 
     test('Should return data if post returns 200 ', () async {
-      mockAnswer(anyAnswer, 200);
+      mockAnswer(200, data: anyAnswer);
       final response = await sut.request(url: url, method: 'post');
       expect(response, {"any_key": "any_value"});
     });
 
     test('Should return null if post returns 200 with no data', () async {
-      mockAnswer(null, 200);
+      mockAnswer(200);
       final response = await sut.request(url: url, method: 'post');
       expect(response, null);
     });
 
     test('Should return null if post returns 204', () async {
-      mockAnswer(null, 204);
+      mockAnswer(204);
       final response = await sut.request(url: url, method: 'post');
       expect(response, null);
     });
 
     test('Should return null if post returns 204 with data', () async {
-      mockAnswer(anyAnswer, 204);
+      mockAnswer(
+        204,
+        data: anyAnswer,
+      );
       final response = await sut.request(url: url, method: 'post');
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockAnswer(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockAnswer(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return UnauthorizedError if post returns 401', () async {
+      mockAnswer(401);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
+    test('Should return ForbiddenError if post returns 403', () async {
+      mockAnswer(403);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.forbidden));
+    });
+
+    test('Should return NotFoundError if post returns 404', () async {
+      mockAnswer(404);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.notFound));
+    });
+
+    test('Should return ServerError if post returns 500', () async {
+      mockAnswer(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if post throws', () async {
+      mockError();
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
     });
   });
 }
