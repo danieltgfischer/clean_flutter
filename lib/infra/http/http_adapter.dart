b5ef4b9f86e7d6ddfb58f9a1f2a8677fecd23fd9
Daniel Fischer <danieltgfischer@gmail.com>
2024-09-01 14:54:35 -0300

    feat: HttpAdapter returns null if succesful answer with no data

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -6,14 +6,18 @@ class HttpAdapter {
   final Client client;
   HttpAdapter(this.client);
 
-  Future<Map> request(
-      {required String url, required String method, Map? body}) async {
+  Future<Map?> request({
+    required String url,
+    required String method,
+    Map? body,
+  }) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
     final response =
         await client.post(Uri.parse(url), headers: headers, body: body);
+    if (response.body.isEmpty) return null;
     return jsonDecode(response.body);
   }
 }
