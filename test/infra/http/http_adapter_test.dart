2bc5fd5de2d7eb811e57160cf424b4840f9d42c3
Daniel Fischer <danieltgfischer@gmail.com>
2024-09-01 12:00:41 -0300

    test: ensure HttpAdapter calls post method with correct URI

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'package:clean_flutter/infra/http/http.dart';
+
+class ClientSpy extends Mock implements Client {}
+
+void main() {
+  late String url;
+  late ClientSpy client;
+  late HttpAdapter sut;
+  late Uri uri;
+
+  setUp(() {
+    url = faker.internet.httpUrl();
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    uri = Uri.parse(url);
+    registerFallbackValue(uri);
+  });
+  group('post', () {
+    setUp(() {
+      when(() => client.post(any())).thenAnswer((_) async => Response('', 200));
+    });
+    test('Should call post with correct values', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(() => client.post(uri));
+    });
+  });
+}
