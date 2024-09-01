6ed20952ab8253da4fad6536df671fb4db6ebd83
Daniel Fischer <danieltgfischer@gmail.com>
2024-08-31 21:28:22 -0300

    feat: prevent AccountModel creates invalid object

--- lib/data/models/account_model.dart
+++ lib/data/models/account_model.dart
@@ -1,10 +1,17 @@
 import '../../domain/entities/entities.dart';
 
-class AccountModel {
-  final String token;
-  AccountModel(this.token);
+import '../http/http.dart';
 
-  factory AccountModel.fromJson(Map json) => AccountModel(json['accessToken']);
+class RemoteAccountModel {
+  final String token;
+  RemoteAccountModel(this.token);
+
+  factory RemoteAccountModel.fromJson(Map<dynamic, dynamic> json) {
+    if (!json.containsKey('accessToken') || json['accessToken'] == null) {
+      throw HttpError.invalidData;
+    }
+    return RemoteAccountModel(json['accessToken']);
+  }
 
   AccountEntity toEntity() => AccountEntity(token);
 }
