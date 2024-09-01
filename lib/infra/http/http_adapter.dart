da50507731ab2083241ea1b4dee9419110f86111
Daniel Fischer <danieltgfischer@gmail.com>
2024-09-01 14:42:10 -0300

    feat: HttpAdapter returns proper data when post is succesful

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -1,15 +1,19 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 
 class HttpAdapter {
   final Client client;
   HttpAdapter(this.client);
 
-  Future<void> request(
+  Future<Map> request(
       {required String url, required String method, Map? body}) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await client.post(Uri.parse(url), headers: headers, body: body);
+    final response =
+        await client.post(Uri.parse(url), headers: headers, body: body);
+    return jsonDecode(response.body);
   }
 }
