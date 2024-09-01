c945a59ffe0b89d6696d79ebfd0592b44349e138
Daniel Fischer <danieltgfischer@gmail.com>
2024-09-01 20:06:57 -0300

    feat: HttpAdaptor throws errors when needed

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -17,15 +17,39 @@ class HttpAdapter implements HttpClient {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    final response =
-        await client.post(Uri.parse(url), headers: headers, body: body);
-    return _handleResponse(response);
+    Response response = Response('', 500);
+    try {
+      if (method == 'post') {
+        response = await client.post(
+          Uri.parse(url),
+          headers: headers,
+          body: body,
+        );
+      }
+      return _handleResponse(response);
+    } on HttpError {
+      rethrow;
+    } catch (e) {
+      throw HttpError.serverError;
+    }
   }
 
   Map? _handleResponse(Response response) {
-    if (response.statusCode == 200) {
-      return response.body.isEmpty ? null : jsonDecode(response.body);
+    switch (response.statusCode) {
+      case 200:
+        return response.body.isEmpty ? null : jsonDecode(response.body);
+      case 204:
+        return null;
+      case 400:
+        throw HttpError.badRequest;
+      case 401:
+        throw HttpError.unauthorized;
+      case 403:
+        throw HttpError.forbidden;
+      case 404:
+        throw HttpError.notFound;
+      default:
+        throw HttpError.serverError;
     }
-    return null;
   }
 }
--- requirements/http/http_responses.md
+++ requirements/http/http_responses.md
@@ -10,20 +10,20 @@
 
 > ## Erros
 
-1. Bad request - 400
-2. Unauthorized - 401
-3. Forbidden - 403
-4. Not found - 404
-5. Internal server error - 500
+1. ✅ Bad request - 400
+2. ✅ Unauthorized - 401
+3. ✅ Forbidden - 403
+4. ✅ Not found - 404
+5. ✅ Internal server error - 500
 
 > ## Exceção - Status code diferente dos citados acima
 
-1. Internal server error - 500
+1. ✅ Internal server error - 500
 
 > ## Exceção - Http request deu alguma exceção
 
-1. Internal server error - 500
+1. ✅ Internal server error - 500
 
 > ## Exceção - Verbo http inválido
 
-1. Internal server error - 500
+1. ✅ Internal server error - 500
