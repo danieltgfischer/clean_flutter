6343c4ebce80bb9a2308684f5fc24cb9637afb04
Daniel Fischer <danieltgfischer@gmail.com>
2024-09-03 19:41:18 -0300

    feat: ensure LoginPage calls auth method and updates ui

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -3,86 +3,142 @@ import 'package:flutter/material.dart';
 
 import '../../components/components.dart';
 
-class LoginPage extends StatelessWidget {
-  late LoginPresenter? presenter;
+class LoginPage extends StatefulWidget {
+  final LoginPresenter? presenter;
 
-  LoginPage(this.presenter, {super.key});
+  const LoginPage(this.presenter, {super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void dispose() {
+    super.dispose();
+    widget.presenter?.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            LoginHeader(),
-            const Headline1(text: 'Login'),
-            Padding(
-              padding: const EdgeInsets.all(32),
-              child: Form(
-                child: Column(
+      body: Builder(builder: (context) {
+        widget.presenter?.isLoadingStream.listen((isLoading) {
+          if (!mounted) return;
+          if (isLoading == true && context.mounted) {
+            showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (context) {
+                return const SimpleDialog(
                   children: <Widget>[
-                    StreamBuilder<String?>(
-                        stream: presenter?.emailErrorStream,
-                        builder: (context, snapshot) {
-                          return TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Email',
-                              icon: Icon(
-                                Icons.email,
-                                color: Theme.of(context).primaryColorLight,
-                              ),
-                              errorText: snapshot.data?.isEmpty == true
-                                  ? null
-                                  : snapshot.data,
-                            ),
-                            onChanged: presenter?.validateEmail,
-                            keyboardType: TextInputType.emailAddress,
-                          );
-                        }),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8, bottom: 32),
-                      child: StreamBuilder<String?>(
-                          stream: presenter?.passwordErrorStream,
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircularProgressIndicator(),
+                        SizedBox(height: 10),
+                        Text('Aguarde...', textAlign: TextAlign.center),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            );
+          } else {
+            if (context.mounted) {
+              if (Navigator.canPop(context)) {
+                Navigator.of(context).pop();
+              }
+            }
+          }
+        });
+
+        widget.presenter?.mainErrorStream.listen((error) {
+          if (error != null && context.mounted) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                backgroundColor: Colors.red[900],
+                content: Text(error, textAlign: TextAlign.center),
+              ),
+            );
+          }
+        });
+
+        return SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              LoginHeader(),
+              const Headline1(text: 'Login'),
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: Form(
+                  child: Column(
+                    children: <Widget>[
+                      StreamBuilder<String?>(
+                          stream: widget.presenter?.emailErrorStream,
                           builder: (context, snapshot) {
                             return TextFormField(
                               decoration: InputDecoration(
-                                labelText: 'Senha',
+                                labelText: 'Email',
                                 icon: Icon(
-                                  Icons.lock,
+                                  Icons.email,
                                   color: Theme.of(context).primaryColorLight,
                                 ),
                                 errorText: snapshot.data?.isEmpty == true
                                     ? null
                                     : snapshot.data,
                               ),
-                              obscureText: true,
-                              onChanged: presenter?.validatePassword,
+                              onChanged: widget.presenter?.validateEmail,
+                              keyboardType: TextInputType.emailAddress,
                             );
                           }),
-                    ),
-                    StreamBuilder<bool?>(
-                        stream: presenter?.isFormValidStream,
-                        builder: (context, snapshot) {
-                          return ElevatedButton(
-                            onPressed: snapshot.data == true ? () {} : null,
-                            child: Text(
-                              'Entrar'.toUpperCase(),
-                            ),
-                          );
-                        }),
-                    TextButton.icon(
-                      onPressed: () {},
-                      icon: const Icon(Icons.person),
-                      label: const Text('Criar Conta'),
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8, bottom: 32),
+                        child: StreamBuilder<String?>(
+                            stream: widget.presenter?.passwordErrorStream,
+                            builder: (context, snapshot) {
+                              return TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: 'Senha',
+                                  icon: Icon(
+                                    Icons.lock,
+                                    color: Theme.of(context).primaryColorLight,
+                                  ),
+                                  errorText: snapshot.data?.isEmpty == true
+                                      ? null
+                                      : snapshot.data,
+                                ),
+                                obscureText: true,
+                                onChanged: widget.presenter?.validatePassword,
+                              );
+                            }),
+                      ),
+                      StreamBuilder<bool?>(
+                          stream: widget.presenter?.isFormValidStream,
+                          builder: (context, snapshot) {
+                            return ElevatedButton(
+                              onPressed: snapshot.data == true
+                                  ? widget.presenter?.auth
+                                  : null,
+                              child: Text(
+                                'Entrar'.toUpperCase(),
+                              ),
+                            );
+                          }),
+                      TextButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(Icons.person),
+                        label: const Text('Criar Conta'),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
+              )
+            ],
+          ),
+        );
+      }),
     );
   }
 }
