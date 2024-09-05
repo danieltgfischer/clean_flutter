303e2c6270c34c3c78fdbd75e02e433872a3d540
Daniel Fischer <danieltgfischer@gmail.com>
2024-09-05 10:58:25 -0300

    fix: typo

--- lib/ui/components/components.dart
+++ lib/ui/components/components.dart
@@ -1,5 +1,5 @@
 export 'app.dart';
 export 'login_header.dart';
 export 'headline1.dart';
-export 'handle_progress_ndicator.dart';
+export 'handle_progress_indicator.dart';
 export 'show_error_message.dart';
